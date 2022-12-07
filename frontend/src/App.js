32a12b00050ed18de83791985ea37497553e23f2
=Malihe Sheidaiefar <m.sheidaiefar@rayanbourse.ir>
2022-12-07 15:12:11 +0330

    signup,login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import Signup from "./components/signup/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
 
 export default App;
